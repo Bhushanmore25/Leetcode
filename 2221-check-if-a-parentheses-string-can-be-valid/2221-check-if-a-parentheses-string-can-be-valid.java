5bb00f1998fe897f4212ac5dd54074dc5e34ddaa
Bhushanmore25 <147221953+Bhushanmore25@users.noreply.github.com>
2025-01-12 12:32:32 +0530

    Time: 10 ms (98.76%), Space: 46.1 MB (19.5%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.java
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.java
@@ -1,25 +1,25 @@
 class Solution {
     public boolean canBeValid(String s, String locked) {
-         int n = s.length();
-    if (n % 2 != 0) return false;
-
-    int open = 0, flexible = 0;
-    for (int i = 0; i < n; i++) {
-        if (locked.charAt(i) == '0') flexible++;
-        else if (s.charAt(i) == '(') open++;
-        else open--;
-        if (open + flexible < 0) return false;
-    }
-
-    int close = 0;
-    flexible = 0;
-    for (int i = n - 1; i >= 0; i--) {
-        if (locked.charAt(i) == '0') flexible++;
-        else if (s.charAt(i) == ')') close++;
-        else close--;
-        if (close + flexible < 0) return false;
-    }
-
-    return true;
+        if((s.length() & 1) == 1) return false;
+        int cmin = 0, cmax = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if(locked.charAt(i) == '0') {
+                cmin--;
+                cmax++;
+            }
+            else {
+                if(s.charAt(i) == '(') {
+                    cmax++;
+                    cmin++;
+                }
+                else {
+                    cmax--;
+                    cmin--;
+                }
+            }
+            if(cmax < 0) return false;
+            cmin = Math.max(cmin, 0);
+        }
+        return cmin == 0;
     }
 }
