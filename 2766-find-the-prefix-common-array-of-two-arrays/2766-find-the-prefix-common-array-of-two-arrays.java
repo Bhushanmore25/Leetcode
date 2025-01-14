5e588392696e8750dc5006253620e25b6d22bb8c
Bhushanmore25 <147221953+Bhushanmore25@users.noreply.github.com>
2025-01-14 07:37:12 +0530

    Time: 4 ms (72.92%), Space: 45.6 MB (40.89%) - LeetHub

--- 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.java
@@ -1,22 +1,24 @@
 class Solution {
     public int[] findThePrefixCommonArray(int[] A, int[] B) {
-        int n=A.length;
-        // int[] prefix=new int[n];
-        Map<Integer,Integer> mpa=new HashMap<>();
-        Map<Integer,Integer> mpb=new HashMap<>();
-        int[] ans=new int[n];
-        for(int i=0;i<n;i++)
-        {
-            mpa.put(A[i],mpa.getOrDefault(A[i],0)+1);
-            mpb.put(B[i],mpb.getOrDefault(B[i],0)+1);
-            int count=0;
-            for (Map.Entry<Integer, Integer> set : mpa.entrySet()) {
-                if(mpb.containsKey(set.getKey()))
-                {
-                    count++;
-                }
+        int n = A.length;
+        int[] ans = new int[n];
+        Set<Integer> seen = new HashSet<>();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (seen.contains(A[i])) {
+                count++;
+            } else {
+                seen.add(A[i]);
             }
-            ans[i]=count;
+
+            if (seen.contains(B[i])) {
+                count++;
+            } else {
+                seen.add(B[i]);
+            }
+
+            ans[i] = count;
         }
         return ans;
     }
