6732839b1527622a08533a55e630ad85e5a5eb31
Bhushanmore25 <147221953+Bhushanmore25@users.noreply.github.com>
2025-02-13 09:43:56 +0530

    Time: 118 ms (100%), Space: 69.2 MB (95.06%) - LeetHub

--- 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.java
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.java
@@ -1,18 +1,27 @@
 class Solution {
-    public int minOperations(int[] nums, int k) {
-        PriorityQueue<Long> pq=new PriorityQueue<>();
-        for(int i:nums)
-        {
-            pq.offer((long)i);
+     public int minOperations(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]<=k)
+                pq.add(nums[i]);
         }
-        int count=0;
-        while(pq.peek()<k && pq.size()>=2)
-        {
-            long x=pq.poll();
-            long y=pq.poll();
-            pq.offer(x*2 + y);
-            count++;
+        int times=0;
+        while(!pq.isEmpty()&&pq.peek()<k){
+
+           int first =  pq.poll();
+           if(pq.isEmpty()){
+            times++;
+            break;
+           }
+           int sec = pq.poll();
+           long num = (long)(first*2) +sec;
+            if(num < k)
+            pq.add((int)num);
+            
+           times++;
         }
-        return count;
+
+        return times;
     }
 }
