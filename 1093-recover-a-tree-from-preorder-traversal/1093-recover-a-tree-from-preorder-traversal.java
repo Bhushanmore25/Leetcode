3d97feb41eaf0d410fac42828a73e2a3ad15f113
Bhushanmore25 <147221953+Bhushanmore25@users.noreply.github.com>
2025-02-22 23:31:09 +0530

    Time: 2 ms (87.91%), Space: 44.7 MB (70.7%) - LeetHub

--- 1093-recover-a-tree-from-preorder-traversal/1093-recover-a-tree-from-preorder-traversal.java
+++ 1093-recover-a-tree-from-preorder-traversal/1093-recover-a-tree-from-preorder-traversal.java
@@ -1,63 +1,44 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
+    private int i=0;
     public TreeNode recoverFromPreorder(String traversal) {
-        int[] pos = new int[1];
-        int n = traversal.length();
-        if (n == 0) return null;
-        int rootVal = getVal(traversal, n, pos);
-        TreeNode root = new TreeNode(rootVal);
-        buildTree(root, 1, traversal, n, pos);
-        buildTree(root, 1, traversal, n, pos);
-        return root;
+        
+        return buildTree(traversal,0);
     }
-    
-    private int getVal(String traversal, int n, int[] pos) {
-        int val = 0;
-        while (pos[0] < n && Character.isDigit(traversal.charAt(pos[0]))) {
-            val = val * 10 + (traversal.charAt(pos[0]) - '0');
-            pos[0]++;
+
+    private TreeNode buildTree(String traversal,int depth) {
+        int dashes=0;
+        while(i + dashes <traversal.length() && traversal.charAt(i + dashes) == '-')
+        {
+            dashes++;
         }
-        return val;
-    }
-    
-    private int getDashLen(String traversal, int n, int[] pos) {
-        int originalPos = pos[0];
-        while (pos[0] < n && traversal.charAt(pos[0]) == '-') {
-            pos[0]++;
+
+        if(dashes != depth)
+        {
+            return null;
         }
-        return pos[0] - originalPos;
-    }
-    
-    private void buildTree(TreeNode curr, int expectedDashLen, String traversal, int n, int[] pos) {
-        if (pos[0] == n) return;
-        int originalPos = pos[0];
-        int dashLen = getDashLen(traversal, n, pos);
-        if (dashLen != expectedDashLen) {
-            pos[0] = originalPos;
-            return;
+
+        i+=depth;
+
+        final int start=i;
+        while(i<traversal.length() && Character.isDigit(traversal.charAt(i)))
+        {
+            i++;
         }
-        int nodeVal = getVal(traversal, n, pos);
-        TreeNode newNode = new TreeNode(nodeVal);
-        if (curr.left == null) {
-            curr.left = newNode;
-        } else {
-            curr.right = newNode;
-        }
-        buildTree(newNode, expectedDashLen + 1, traversal, n, pos);
-        buildTree(newNode, expectedDashLen + 1, traversal, n, pos);
+        return new TreeNode(Integer.valueOf(traversal.substring(start,i)), buildTree(traversal,depth+1),buildTree(traversal,depth+1));
     }
 }
