bc328d93ed2ef73a0d86f3c0df46ed49fa00b1ca
Bhushanmore25 <147221953+Bhushanmore25@users.noreply.github.com>
2025-03-21 18:41:55 +0530

    Time: 455 ms (11.26%), Space: 47.3 MB (24.28%) - LeetHub

--- 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.java
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 class Solution {
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
-        HashSet<String> available = new HashSet<>(Arrays.asList(supplies)); // Store initial supplies
+        HashSet<String> available = new HashSet<>(Arrays.asList(supplies)); 
         List<String> result = new ArrayList<>();
-        boolean addedNewRecipe = true; // To track if we are making progress
+        boolean addedNewRecipe = true;
 
         while (addedNewRecipe) {
             addedNewRecipe = false;
             for (int i = 0; i < recipes.length; i++) {
-                if (available.contains(recipes[i])) continue; // Skip if already added
+                if (available.contains(recipes[i])) continue; 
                 
                 boolean canMake = true;
                 for (String ing : ingredients.get(i)) {
@@ -21,7 +21,7 @@ class Solution {
                 
                 if (canMake) {
                     result.add(recipes[i]);
-                    available.add(recipes[i]); // Mark recipe as available
+                    available.add(recipes[i]); 
                     addedNewRecipe = true;
                 }
             }
