db426919fae4859870e4e91f479ede29beca3fba
Bhushanmore25 <147221953+Bhushanmore25@users.noreply.github.com>
2025-03-30 12:11:42 +0530

    Time: 4 ms (92.83%), Space: 42.5 MB (43.2%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.java
+++ 0768-partition-labels/0768-partition-labels.java
@@ -1,18 +1,18 @@
 class Solution {
   public List<Integer> partitionLabels(String s) {
-    char[] str = s.toCharArray();
+    
 
     int[] lastSeen = new int[26];
-    for(int i = 0; i < str.length; i++) {
-      lastSeen[str[i] - 'a'] = i;
+    for(int i = 0; i < s.length(); i++) {
+      lastSeen[s.charAt(i) - 'a'] = i;
     }
 
     int left = 0, right = 0, prev = 0;
     List<Integer> result = new LinkedList<>();
-    while(left < str.length) {
-      right = lastSeen[str[left] - 'a'];
+    while(left < s.length()) {
+      right = lastSeen[s.charAt(left) - 'a'];
       while(left <= right) {
-        right = Math.max(right, lastSeen[str[left] - 'a']);
+        right = Math.max(right, lastSeen[s.charAt(left) - 'a']);
         left++;
       }
       result.add(right+1-prev);
