22250dcbae3c27df48d267c07667221917aa509f
Bhushanmore25 <147221953+Bhushanmore25@users.noreply.github.com>
2025-03-31 23:07:23 +0530

    Time: 0 ms (100%), Space: 40.2 MB (92%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.java
+++ 0397-integer-replacement/0397-integer-replacement.java
@@ -17,3 +17,26 @@ class Solution {
         return count;
     }
 }
+// class Solution {
+//   Map<Integer, Integer> results = new HashMap<>();
+
+//   public int integerReplacement(int n) {
+//     if (n == 1) {
+//       return 0;
+//     }
+//     if (n == Integer.MAX_VALUE) {
+//       return 32;
+//     }
+//     if (!results.containsKey(n)) {
+//       if (n % 2 == 0) {
+//         results.put(n, integerReplacement(n / 2));
+//       } else {
+//         // n is odd
+//         results.put(n, Math.min(
+//             integerReplacement(n - 1),
+//             integerReplacement(n + 1)));
+//       }
+//     }
+//     return 1 + results.get(n);
+//   }
+// }
