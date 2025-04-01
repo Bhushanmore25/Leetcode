0d383df850221fb3050ee17c9b72401c374bcc99
Bhushanmore25 <147221953+Bhushanmore25@users.noreply.github.com>
2025-04-01 22:20:24 +0530

    Time: 7 ms (53.04%), Space: 108.2 MB (65.18%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.java
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.java
@@ -1,23 +1,33 @@
 import java.util.Arrays;
 
 class Solution {
-    public static long recur(int[][] questions, long[] dp, int idx, int m) {
-        if (idx >= m) {
-            return 0L;
-        }
-        if (dp[idx] != -1) return dp[idx];
+    // public static long recur(int[][] questions, long[] dp, int idx, int m) {
+    //     if (idx >= m) {
+    //         return 0L;
+    //     }
+    //     if (dp[idx] != -1) return dp[idx];
 
-        long take = questions[idx][0] + recur(questions, dp, idx + questions[idx][1] + 1, m);
-        long notTake = recur(questions, dp, idx + 1, m);
+    //     long take = questions[idx][0] + recur(questions, dp, idx + questions[idx][1] + 1, m);
+    //     long notTake = recur(questions, dp, idx + 1, m);
         
-        return dp[idx] = Math.max(take, notTake);
-    }
+    //     return dp[idx] = Math.max(take, notTake);
+    // }
 
     public long mostPoints(int[][] questions) {
         int m = questions.length;
         long[] dp = new long[m];
         Arrays.fill(dp, -1);
 
-        return recur(questions, dp, 0, m);
+        for (int idx = m - 1; idx >= 0; idx--) 
+        { 
+            long take = questions[idx][0]; 
+            if (idx + questions[idx][1] + 1 < m) { 
+                take += dp[idx + questions[idx][1] + 1]; 
+            }
+            long notTake = (idx + 1 < m) ? dp[idx + 1] : 0; 
+            dp[idx] = Math.max(take, notTake);
+        }
+
+        return dp[0];
     }
 }
