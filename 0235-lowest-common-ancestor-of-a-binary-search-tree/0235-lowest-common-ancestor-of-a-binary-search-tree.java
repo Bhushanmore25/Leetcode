8599023104b863e70ac06d8f290211642fb327d6
Bhushanmore25 <147221953+Bhushanmore25@users.noreply.github.com>
2025-04-04 22:26:19 +0530

    Time: 5 ms (100%), Space: 45.3 MB (37.91%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,42 +9,42 @@
  */
 
 class Solution {
-    public static boolean dfs(TreeNode root,TreeNode target,List<TreeNode> path)
-    {
-        if(root==null)
-        {
-            return false;
-        }
-        path.add(root);
-        if(root.val==target.val)
-        {
-            return true;
-        }
-        boolean left=dfs(root.left,target,path);
-        boolean right=dfs(root.right,target,path);
-        if(left || right) return true;
-        path.remove(path.size()-1);
-        return false;
-    }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        List<TreeNode> path1=new ArrayList<>();
-        List<TreeNode> path2=new ArrayList<>();
-        dfs(root,p,path1);
-        dfs(root,q,path2);
-        int i=0;
-        while (i < path1.size() && i < path2.size() && path1.get(i).val == path2.get(i).val) {
-            i++;
+        if(root == null) return null;
+        if (root.val > p.val && root.val > q.val){
+            return lowestCommonAncestor(root.left,p,q);
         }
-        return path1.get(i - 1);
+        if (root.val < p.val && root.val < q.val){
+            return lowestCommonAncestor(root.right,p,q);
+        }
+        return root;
     }
 }
+// public static boolean dfs(TreeNode root,TreeNode target,List<TreeNode> path)
+// {
+//     if(root==null)
+//     {
+//         return false;
+//     }
+//     path.add(root);
+//     if(root.val==target.val)
+//     {
+//         return true;
+//     }
+//     boolean left=dfs(root.left,target,path);
+//     boolean right=dfs(root.right,target,path);
+//     if(left || right) return true;
+//     path.remove(path.size()-1);
+//     return false;
+// }
 // public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-//     if(root == null) return null;
-//     if (root.val > p.val && root.val > q.val){
-//         return lowestCommonAncestor(root.left,p,q);
+//     List<TreeNode> path1=new ArrayList<>();
+//     List<TreeNode> path2=new ArrayList<>();
+//     dfs(root,p,path1);
+//     dfs(root,q,path2);
+//     int i=0;
+//     while (i < path1.size() && i < path2.size() && path1.get(i).val == path2.get(i).val) {
+//         i++;
 //     }
-//     if (root.val < p.val && root.val < q.val){
-//         return lowestCommonAncestor(root.right,p,q);
-//     }
-//     return root;
+//     return path1.get(i - 1);
 // }
