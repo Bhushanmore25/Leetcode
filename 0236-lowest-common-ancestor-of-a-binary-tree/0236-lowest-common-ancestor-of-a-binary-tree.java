a45a52c462572dff7769566458226c4f9fb25613
Bhushanmore25 <147221953+Bhushanmore25@users.noreply.github.com>
2025-04-04 22:35:05 +0530

    Time: 9 ms (15.66%), Space: 44.6 MB (85.16%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public static boolean dfs(TreeNode root,TreeNode target,List<TreeNode> path)
+    {
+        if(root==null)
+        {
+            return false;
+        }
+        path.add(root);
+        if(root.val==target.val)
+        {
+            return true;
+        }
+        boolean left=dfs(root.left,target,path);
+        boolean right=dfs(root.right,target,path);
+        if(left || right) return true;
+        path.remove(path.size()-1);
+        return false;
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        List<TreeNode> path1=new ArrayList<>();
+        List<TreeNode> path2=new ArrayList<>();
+        dfs(root,p,path1);
+        dfs(root,q,path2);
+        int i=0;
+        while (i < path1.size() && i < path2.size() && path1.get(i).val == path2.get(i).val) {
+            i++;
+        }
+        return path1.get(i - 1);
+    }
+    // public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+    // }
+}
