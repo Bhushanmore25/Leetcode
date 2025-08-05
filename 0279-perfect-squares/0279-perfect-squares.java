fba4b564f9104c08d34e872c31422fb2d558f554
Bhushanmore25 <147221953+Bhushanmore25@users.noreply.github.com>
2025-08-05 22:36:56 +0530

    Time: 64 ms (32.93%), Space: 43.3 MB (35.02%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,30 +1,56 @@
+// class Solution {
+//     public int numSquares(int n) {
+//         Queue<Integer> queue = new LinkedList<>();
+//         Set<Integer> visited = new HashSet<>();
+
+//         queue.offer(n);
+//         visited.add(n);
+//         int level = 0;
+
+//         while (!queue.isEmpty()) {
+//             int size = queue.size();
+//             level++;
+
+//             for (int i = 0; i < size; i++) {
+//                 int curr = queue.poll();
+
+//                 for (int j = 1; j * j <= curr; j++) {
+//                     int next = curr - j * j;
+//                     if (next == 0) return level;
+//                     if (!visited.contains(next)) {
+//                         queue.offer(next);
+//                         visited.add(next);
+//                     }
+//                 }
+//             }
+//         }
+
+//         return level; 
+//     }
+// }
+// Time complexity: O(N * sqrt(N))
+// Space complexity: O(N)
+	
 class Solution {
     public int numSquares(int n) {
-        Queue<Integer> queue = new LinkedList<>();
-        Set<Integer> visited = new HashSet<>();
-
-        queue.offer(n);
-        visited.add(n);
-        int level = 0;
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            level++;
-
-            for (int i = 0; i < size; i++) {
-                int curr = queue.poll();
-
-                for (int j = 1; j * j <= curr; j++) {
-                    int next = curr - j * j;
-                    if (next == 0) return level;
-                    if (!visited.contains(next)) {
-                        queue.offer(next);
-                        visited.add(next);
-                    }
-                }
-            }
+        int[] memo = new int[n + 1];
+        return helper(n, memo);
+    }
+    
+    public int helper(int n, int[] memo) {
+        if (n < 4)
+            return n;
+        
+        if (memo[n] != 0)
+            return memo[n];
+        
+        int ans = n;
+        
+        for (int i = 1; i * i <= n; i++) {
+            int square = i * i;
+            ans = Math.min(ans, 1 + helper(n - square, memo));
         }
-
-        return level; 
+        
+        return memo[n] = ans;
     }
 }
