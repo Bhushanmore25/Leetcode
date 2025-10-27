f46b31e1744f5dee32d64ac0a4727871e10328fa
Bhushanmore25 <147221953+Bhushanmore25@users.noreply.github.com>
2025-10-28 00:08:03 +0530

    Time: 12 ms (53.73%), Space: 45 MB (76.08%) - LeetHub

--- 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.java
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.java
@@ -1,27 +1,21 @@
 class Solution {
     public int numberOfBeams(String[] bank) {
-        List<Integer> li=new ArrayList<>();
-        for(int i = bank.length-1; i >= 0 ;i-- )
-        {
-            int c=0;
-            for(char ch:bank[i].toCharArray()) if(ch=='1') c++;
-            li.add(c);
-        }
-        int ans=0,n = li.size();
-        for(int i=0;i<n;)
-        {
-            if (li.get(i) == 0) { 
-                i++;
-                continue;
+        List<Integer> devices = new ArrayList<>();
+        
+        for (String row : bank) {
+            int count = 0;
+            for (char ch : row.toCharArray()) {
+                if (ch == '1') count++;
             }
-            int j=i+1;
-            while(j<n && li.get(j)==0) j++;
-            if(j==n) break;
-            int x=li.get(i)*li.get(j);
-            System.out.println(x);
-            ans+=x;
-            i=j;
+            if (count > 0) devices.add(count);
         }
+
+        int ans = 0;
+        for (int i = 1; i < devices.size(); i++) {
+            ans += devices.get(i - 1) * devices.get(i);
+        }
+
         return ans;
+
     }
 }
