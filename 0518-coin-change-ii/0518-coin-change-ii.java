7d1541b27bca14e64a11e9f0c8d8e95ffaedaf16
Bhushanmore25 <147221953+Bhushanmore25@users.noreply.github.com>
2025-11-05 12:40:11 +0530

    Time: 13 ms (37.38%), Space: 56.3 MB (5.89%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,20 +1,32 @@
 class Solution {
-    public int[][] dp= new int[301][5001];
-    public static int ways(int[][] dp,int amount,int[] coins,int idx)
-    {
-        if(amount==0) return 1;
-        if(idx<0) return 0;
-        if(dp[idx][amount]!=-1) return dp[idx][amount];
-        int ans=0;
-        for(int i=0;i<=amount;i+=coins[idx])
-        {
-            ans+=ways(dp,amount-i,coins,idx-1);
-        }
-        return dp[idx][amount]=ans;
-    }
+    
+    // public static int ways(int[][] dp,int amount,int[] coins,int idx)
+    // {
+    //     if(amount==0) return 1;
+    //     if(idx<0) return 0;
+    //     if(dp[idx][amount]!=-1) return dp[idx][amount];
+    //     int ans=0;
+    //     for(int i=0;i<=amount;i+=coins[idx])
+    //     {
+    //         ans+=ways(dp,amount-i,coins,idx-1);
+    //     }
+    //     return dp[idx][amount]=ans;
+    // }
     public int change(int amount, int[] coins) {
-        for(int i[]:dp)
-            Arrays.fill(i,-1);
-        return ways(dp,amount,coins,coins.length-1);
+        int[][] dp=new int[coins.length+1][amount+1];
+        for(int i=0;i<coins.length;i++) dp[i][0]=1;
+        int n=coins.length;
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int sum=1;sum<=amount;sum++)
+            {
+                int notpick=dp[i+1][sum];
+                int pick=0;
+                if(coins[i]<=sum)
+                    pick=dp[i][sum-coins[i]];
+                dp[i][sum]= pick + notpick;
+            }
+        }
+        return dp[0][amount];
     }
 }
