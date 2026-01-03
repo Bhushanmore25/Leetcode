c2b255f6cd0da23b4966cd840ec578b60620f124
Bhushanmore25 <147221953+Bhushanmore25@users.noreply.github.com>
2026-01-03 13:27:50 +0530

    Time: 15 ms (99.42%), Space: 52.7 MB (67.87%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.java
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.java
@@ -3,9 +3,26 @@ class Solution {
     public String kthLargestNumber(String[] nums, int k) {
         Arrays.sort(nums, (a, b) -> {
             if (a.length() != b.length())
-                return a.length() - b.length(); // shorter first
-            return a.compareTo(b); // lexicographically
+                return a.length() - b.length(); 
+            return a.compareTo(b); 
         });
         return nums[nums.length - k];
     }
 }
+// import java.math.BigInteger;
+// import java.util.PriorityQueue;
+
+// class Solution {
+//     public String kthLargestNumber(String[] nums, int k) {
+//         var pq = new PriorityQueue<BigInteger>();
+
+//         for (String s : nums) {
+//             pq.add(new BigInteger(s)); 
+//             if (pq.size() > k) {
+//                 pq.poll();
+//             }
+//         }
+
+//         return pq.peek().toString();
+//     }
+// }
