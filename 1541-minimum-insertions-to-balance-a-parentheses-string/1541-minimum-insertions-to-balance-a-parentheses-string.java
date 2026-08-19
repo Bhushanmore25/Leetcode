1543047430173ba096a7e77245f7aedecdf0ee1b
Bhushanmore25 <147221953+Bhushanmore25@users.noreply.github.com>
2026-08-19 21:30:52 +0530

    Time: 30 ms (10.25%), Space: 47.6 MB (43.34%) - LeetHub

---
+++ 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minInsertions(String s) {
+        Stack<Character> st=new Stack<>();
+        int count=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='(')
+            {
+                st.push(s.charAt(i));
+            }
+            else{
+                if(st.isEmpty()){
+                    if(i!=s.length()-1 && s.charAt(i+1)==')')
+                    {
+                        i++;
+                        count++;
+                    }
+                    else{
+                        count+=2;
+                    }
+                }
+                else{
+                    if(i!=s.length()-1 && s.charAt(i+1)==')'){ 
+                        st.pop();
+                        i++;
+                    }
+                    else {
+                        count++;
+                        st.pop();
+                    }
+                }
+            }
+        }
+        return count+st.size()*2;
+    }
+}
