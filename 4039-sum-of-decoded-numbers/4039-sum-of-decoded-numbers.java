e9d9c46c3fd6bed13c1f581431957d7242c3e84f
Bhushanmore25 <147221953+Bhushanmore25@users.noreply.github.com>
2026-08-31 19:55:41 +0530

    Time: 122 ms (66.67%), Space: 88 MB (100%) - LeetHub

---
+++ 4039-sum-of-decoded-numbers/4039-sum-of-decoded-numbers.java
@@ -0,0 +1,34 @@
+class Solution {
+    private static final long MOD = 1_000_000_007L;
+
+    public int sumDecoded(long[] nums) {
+        long ans = 0;
+
+        for (long num : nums) {
+            int width = (int) (num % 10);
+            long d = num / 10;
+
+            String dStr = String.valueOf(d);
+            long x = Long.parseLong(dStr.substring(0, width));
+            long y = Long.parseLong(dStr.substring(width));
+            ans = (ans + power(x, y)) % MOD;
+        }
+
+        return (int) ans;
+    }
+
+    private long power(long base, long exp) {
+        long res = 1;
+        base %= MOD;
+
+        while (exp > 0) {
+            if ((exp & 1) == 1) {
+                res = (res * base) % MOD;
+            }
+            base = (base * base) % MOD;
+            exp >>= 1;
+        }
+
+        return res;
+    }
+}
